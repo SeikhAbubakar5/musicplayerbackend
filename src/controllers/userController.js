def8b2c885950d4e45c79e49a5a19118d3a843ff
Seikh Abubakar <128287400+SeikhAbubakar5@users.noreply.github.com>
2024-08-22 12:11:56 +0530

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,33 +1,32 @@
-const userService = require("../services/userService");
+const userService= require("../services/userService")
 
-const registerUser = async (req, res) => {
+const registerUser=async(req,res)=>{
     try {
-        const userData = req.body;
+        const userData=req.body;
 
-        const user = await userService.registerUser(userData);
+        const user=await userService.registerUser(userData);
 
         res.status(201).json({
-            message: "Successfully registered",
-            userId: user._id // Returning only the user ID
-        });
+            message:"successfully registered",
+            userId:user
+        })
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(500).json({message:error.message})
     }
-};
+}
 
-const loginUser = async (req, res) => {
+const loginUser = async(req,res)=>{
     try {
-        const userData = req.body;
+        const userData=req.body;
 
-        const { token, userId } = await userService.loginUser(userData);
+        const {token ,userId}=await userService.loginUser(userData);
         res.status(200).json({
-            message: "User successfully logged in",
-            token,
-            userId
-        });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
+            message:"user successfully logged",
+            token,userId
+        })
 
-module.exports = { registerUser, loginUser };
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
+module.exports={registerUser ,loginUser};
