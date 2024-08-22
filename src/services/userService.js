91d7271e7d441a9a91f5e09bdd74322a98c2e82c
Seikh Abubakar <128287400+SeikhAbubakar5@users.noreply.github.com>
2024-08-22 11:41:23 +0530

    Update userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,43 +1,44 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const User =require("../models/User")
-const bcrypt=require("bcryptjs");
-const jwt=require("jsonwebtoken")
-const registerUser=async(userData)=>{
+const registerUser = async (userData) => {
     try {
-        const prevUser=await User.findOne({email:userData.email});
-        if(prevUser){
-            throw new Error("User already exists")
+        const prevUser = await User.findOne({ email: userData.email });
+        if (prevUser) {
+            throw new Error("User already exists");
         }
 
-        const user= new User(userData)
-        const salt=await bcrypt.genSalt(10)
-        const hashedPassword= await bcrypt.hash(userData.password,salt)
-        user.password=hashedPassword;
-        user.save()
+        const user = new User(userData);
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(userData.password, salt);
+        user.password = hashedPassword;
+        await user.save(); // Await the save operation
 
         return user;
     } catch (error) {
-        throw error
+        throw error;
     }
-}
+};
 
-const loginUser=async(userData)=>{
+const loginUser = async (userData) => {
     try {
-        const {email ,password}= userData;
-        const user=await User.findOne({email});
-        if(!user){
-            throw new Error("user not exist")
-        }
-        isMatchPassword=await user.comparePassword(password);
-        if(!isMatchPassword){
-            throw new Error("invalid credintials")
+        const { email, password } = userData;
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new Error("User does not exist");
         }
 
-        const token =jwt.sign({id:user._id} , process.env.JWT_SECRET)
-        return {token ,user}
+        const isMatchPassword = await user.comparePassword(password);
+        if (!isMatchPassword) {
+            throw new Error("Invalid credentials");
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' }); // Added expiration
+        return { token, userId: user._id }; // Return user ID instead of the full user object
     } catch (error) {
-        throw error
+        throw error;
     }
-}
+};
 
-module.exports={registerUser ,loginUser}
+module.exports = { registerUser, loginUser };
